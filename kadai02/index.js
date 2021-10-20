3dd793a0b4ba6b4f1c6eca95c22175341a771ea2
Chinakandori <Chinakandori@users.noreply.github.com>
2021-10-20 04:58:53 +0000

    課題２

---
+++ kadai02/index.js
@@ -0,0 +1,11 @@
+const familyname='nanami'
+const first_name='yachiyo'
+console.log(familyname+first_name)
+
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+const dog=false
+const cat=true
+console.log(dog||cat)
+console.log(dog && cat)
+console.log(!dog&& !cat) //!(dog||cat)
